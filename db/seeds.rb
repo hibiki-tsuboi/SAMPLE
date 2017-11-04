0b12da474723b156ebd56bec182da640033e771c
hibiki-tsuboi <hibiki.tsuboi@gmail.com>
2017-11-04 13:28:42 +0900

    deviseの日本語化

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Staff.delete_all
+staff = Staff.new(email: 'admin@admin.jp', password: 'admin00')
+staff.skip_confirmation!
+staff.save!
