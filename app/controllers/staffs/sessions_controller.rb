ec0ea4dc4b140086a5adcb3f8f3b57326f97e5de
hibiki-tsuboi <hibiki.tsuboi@gmail.com>
2017-11-07 14:50:37 +0900

    ログイン後とログイアウト後の遷移先を指定

--- app/controllers/staffs/sessions_controller.rb
+++ app/controllers/staffs/sessions_controller.rb
@@ -1,6 +1,16 @@
 class Staffs::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  # ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # ログアウト後の遷移先
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
