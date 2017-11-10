6dc402d676ee9fb26cdf8c2e042db94a8c0f8bc5
hibiki-tsuboi <hibiki.tsuboi@gmail.com>
2017-11-10 21:36:03 +0900

    unicornの設定を変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,29 @@
-# vim config/unicorn.rb
-rails_root = File.expand_path('../../', __FILE__)
-rails_env = ENV['RAILS_ENV'] || "development"
+# -*- coding: utf-8 -*-
+worker_processes 4
 
-worker_processes 2
-working_directory rails_root
+listen File.expand_path('tmp/unicorn.sock')
+pid File.expand_path('tmp/unicorn.pid')
 
-#listen "#{rails_root}/tmp/#{rails_env}_unicorn.sock"
-listen 8080
-pid "#{rails_root}/tmp/#{rails_env}_unicorn.pid"
+stderr_path File.expand_path('log/unicorn.log')
+stdout_path File.expand_path('log/unicorn.log')
 
-stderr_path "#{rails_root}/log/#{rails_env}_unicorn_error.log"
-stdout_path "#{rails_root}/log/#{rails_env}_unicorn.log"
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+end
