38a79c48d3aded0facd1403f354f00ccb4e6881d
hibiki-tsuboi <hibiki.tsuboi@gmail.com>
2017-11-11 22:12:39 +0900

    unicornの設定を再度、変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
-worker_processes 4
 
-listen File.expand_path('tmp/unicorn.sock')
-pid File.expand_path('tmp/unicorn.pid')
+# Unicornのプロセスがリスンするアドレスとポートを指定
+listen "127.0.0.1:3000"
 
-stderr_path File.expand_path('log/unicorn.log')
-stdout_path File.expand_path('log/unicorn.log')
+# pid fileの位置を指定する
+pid "tmp/pids/unicorn.pid"
 
+# ワーカーの数を指定する
+worker_processes 2
+
+# リクエストのタイムアウト秒を指定する
+timeout 15
+
+# ダウンタイムを無くすため、アプリをプレロード
 preload_app true
 
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+# Unicornのログ出力先を指定
+ROOT = File.dirname(File.dirname(__FILE__))
+stdout_path "#{ROOT}/log/unicorn-stdout.log"
+stderr_path "#{ROOT}/log/unicorn_stderr.log"
 
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
