81476b5dc4bb2b41121648d7f448ded430cc4f91
hibiki-tsuboi <hibiki.tsuboi@gmail.com>
2017-11-11 23:51:47 +0900

    produtionモードでpublic見れないので緊急措置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # TODO：nginx側でpublicが見れるように設定すること
+  config.public_file_server.enabled = true
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
