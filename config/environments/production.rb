797c8cfea04e9d1de109d21a9adaf6b52159c890
hibiki-tsuboi <hibiki.tsuboi@gmail.com>
2017-11-11 23:07:10 +0900

    nginxをセットアップしたのでTODO解消

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # TODO：webサーバ立ち上げたらfalseに変更すること
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -90,6 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # TODO：webサーバ立ち上げたらコメントアウトすること
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
 end
